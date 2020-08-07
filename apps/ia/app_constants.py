2f50e52f29b96f80c3fecf2f8d5a99adfd9feaf4
Danilo Santana <daniloxc@msn.com>
2020-08-07 09:37:44 -0300

    Calculando disponibilidade das Lampadas

---
+++ apps/ia/app_constants.py
@@ -0,0 +1,35 @@
+
+PREDICTOR = {
+    'iluminacao_column_names': [
+        "Id",
+        "Idra",
+        "IdLogradouro",
+        "IdPoste",
+        'poste_numero',
+        'coordenadaX',
+        'coordenadaY',
+        'vlsavis',
+        'referencia',
+        'poste_propriedade',
+        'poste_status',
+        'id_suporte',
+        'IdLuminaria',
+        'luminaria_numero',
+        'altura_montagem,'
+        'IdLampada',
+        'quantidade_lampada',
+        'id_reator',
+        'id_rele',
+        'id_rede',
+        'id_transformador',
+        'id_medidor',
+        'id_objecto_iluminado',
+        'data_instalacao',
+        'observacoes',
+        'incluido_em',
+        'id_usuario',
+        'created_at',
+        'update_at',
+        'deleted_at'
+    ]
+}
